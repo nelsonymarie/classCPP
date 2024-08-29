83b6637b6b42640c44c54f13b256c4dc2b0bde4e
Misael Cuadrado <104469677+nelsonymarie@users.noreply.github.com>
2024-08-29 11:08:59 -0500

    Updated main.cpp and added global .gitignore

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
+//************************************************************************
+// Author: Misael Cuadrado
+// Date: 29 AUG 2024
+// Language: C++
+// Assignment: Module 2 Lab 3
+// Description: Program displays a message to the console to test and make
+// the compiler is working - traditional Hello World
+//************************************************************************
+#include<iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    // declare local variables
+
+    // display message to the user
+    cout << "Hello, World!\n";
+
     return 0;
 }
